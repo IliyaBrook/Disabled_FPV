a28ca8bb78ae9bc46753c106ebb5d7d445b50364
Iliya Brook <iliyabrook1987@gmail.com>
2024-11-17 23:46:56 +0200

    .

--- client/src/app/[lang]/dictionaries.ts
+++ client/src/app/[lang]/dictionaries.ts
@@ -1,10 +1,11 @@
 import "server-only";
+import type { lang } from "@/types/sharable";
 
-const dictionaries: Record<"en" | "he", () => Promise<{ welcome: string; description: string }>> = {
+const dictionaries: Record<lang, () => Promise<{ welcome: string; description: string }>> = {
 	en: () => import("./dictionaries/en.json").then((module) => module.default),
 	he: () => import("./dictionaries/he.json").then((module) => module.default),
 };
 
-export const getDictionary = async (locale: "en" | "he"): Promise<{ welcome: string; description: string }> => {
-	return dictionaries[locale] ? dictionaries[locale]() : dictionaries["en"]();
+export const getDictionary = async (locale: lang): Promise<{ welcome: string; description: string }> => {
+	return dictionaries[locale]();
 };
