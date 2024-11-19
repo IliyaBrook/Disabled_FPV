10e005a710fac6c46694fc95e69452fef23fe746
Iliya Brook <iliyabrook1987@gmail.com>
2024-11-19 19:50:08 +0200

    dynamic routes

--- client/src/app/dictionaries.ts
+++ client/src/app/dictionaries.ts
@@ -1,10 +1,14 @@
-import 'server-only'
-import type { TLangOptions } from '@/app/types/internationalization'
-
+import 'server-only';
+import type { TLangOptions } from '@/app/types/internationalization';
 
 const dictionaries = {
-	en: () => import('./dictionaries/en.json').then((module) => module.default),
-	he: () => import('./dictionaries/he.json').then((module) => module.default),
-}
+	'en-US': () => import('./dictionaries/en.json').then((module) => module.default),
+	'he-IL': () => import('./dictionaries/he.json').then((module) => module.default),
+};
 
-export const getDictionary = async (locale: TLangOptions) => dictionaries[locale]()
+export const getDictionary = async (locale: TLangOptions) => {
+	if (!dictionaries[locale]) {
+		throw new Error(`Dictionary for locale "${locale}" not found`);
+	}
+	return await dictionaries[locale]();
+};
