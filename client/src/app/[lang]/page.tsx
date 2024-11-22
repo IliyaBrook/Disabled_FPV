44938d520c20c9fc9fcb484e710ec3bd19ef987c
Iliya Brook <iliyabrook1987@gmail.com>
2024-11-22 22:09:01 +0200

    footer LinkIcon component

--- client/src/app/[lang]/page.tsx
+++ client/src/app/[lang]/page.tsx
@@ -1,8 +1,8 @@
-import { useAppSelector } from '@/app/store/hooks'
 import type { TLangOptions } from '@/app/types/local.types'
 import { getDictionary } from '@/app/utils/dictionaries'
 import React from 'react'
 import styles from './homePage.module.scss'
+import Image from 'next/image'
 
 export default async function HomePage({
   params,
@@ -12,142 +12,15 @@ export default async function HomePage({
   const p = await params
   const dict = getDictionary(p.lang)
 
-  // const { dict, dir, lang } = useAppSelector((state) => state.localization)
   return (
     <div className={styles.homePage}>
-      <div>
-        <h1>
-          TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP
-        </h1>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-      </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet
-          culpa dignissimos molestias obcaecati placeat veritatis? At
-          consequatur deleniti in libero magni, molestiae odit quam quidem
-          recusandae sed veritatis voluptatem.100
-        </h1>
-        <h1>
-          BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM
-        </h1>
+      <div className={styles.bgImage}>
+        <Image
+          src="/img/man_with_drone_main.jpg"
+          alt="man controls drone picture"
+          fill
+          priority
+        />
       </div>
     </div>
   )
