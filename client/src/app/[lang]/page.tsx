b992465b8b47d053c41bcef2979302c74c2bbe09
Iliya Brook <iliyabrook1987@gmail.com>
2024-11-23 15:45:20 +0200

    footer LinkIcon component

--- client/src/app/[lang]/page.tsx
+++ client/src/app/[lang]/page.tsx
@@ -1,4 +1,5 @@
-import type { TLangOptions } from '@/app/types/local.types'
+import ButtonWithArrow from '@/app/components/ButtonWithArrow/ButtonWithArrow'
+import type { TDir, TLangOptions } from '@/app/types/local.types'
 import { getDictionary } from '@/app/utils/dictionaries'
 import Image from 'next/image'
 import React from 'react'
@@ -11,6 +12,7 @@ export default async function HomePage({
 }): Promise<React.ReactElement> {
   const p = await params
   const dict = await getDictionary(p.lang)
+  const dir: TDir = p.lang === 'he' ? 'rtl' : 'ltr'
   return (
     <div className={styles.homePage}>
       <Image
@@ -22,11 +24,19 @@ export default async function HomePage({
       />
       <div className={styles.content}>
         <div className={styles.textSection}>
+          <div className={styles.welcomeText}>
+            <span className={styles.violetText}>{dict['welcome']}</span>
+            <span className={styles.blueText}>{dict['Disabled FPV']}</span>
+            <span className={styles.violetText}>{dict['online courses']}</span>
+          </div>
           <h1>{dict.main_page_header_text}</h1>
           <p>{dict.main_page_text}</p>
-          <button className={styles.buttonWithArrow}>
-            {dict['Find courses']}
-          </button>
+          <ButtonWithArrow
+            className={styles.buttonWithArrow}
+            title={dict['Find courses']}
+            destination={`/${p.lang}/courses`}
+            dir={dir}
+          />
         </div>
         <div className={styles.withDroneContainer}>
           <Image
--- client/src/app/components/ButtonWithArrow/ButtonWithArrow.tsx
+++ client/src/app/components/ButtonWithArrow/ButtonWithArrow.tsx
@@ -2,28 +2,51 @@
 import type { TDir } from '@/app/types/local.types'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { type CSSProperties } from 'react'
 import styles from './ButtonWithArrow.module.scss'
 
 interface IButtonProps {
-  destination: string
-  title: string
-  dir: TDir
+  destination?: string
+  title?: string
+  dir?: TDir
   className?: string
+  logic?: 'link' | 'onClick'
+  backgroundColor?: CSSProperties['backgroundColor']
+  backgroundHoverColor?: CSSProperties['backgroundColor']
+  style?: CSSProperties
+  onClick?: (props: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const ButtonWithArrow = ({
-  destination,
+  destination = '',
   title,
   dir,
   className,
+  logic = 'link',
+  onClick,
+  backgroundColor,
+  backgroundHoverColor,
+  style = {},
 }: IButtonProps): React.ReactElement => {
   const router = useRouter()
   return (
     <button
+      onClick={(props) =>
+        logic === 'link'
+          ? router.push(destination)
+          : onClick
+            ? onClick(props)
+            : null
+      }
+      style={
+        {
+          '--background-color': backgroundColor,
+          '--background-hover-color': backgroundHoverColor,
+          ...style,
+        } as React.CSSProperties
+      }
       className={`${styles.buttonWithArrow}
       ${className} ${dir === 'rtl' ? styles.rtlStyle : {}}`}
-      onClick={() => router.push(destination)}
     >
       <span className={styles.title}>{title}</span>
       <span className={styles.arrowWrapper}>
